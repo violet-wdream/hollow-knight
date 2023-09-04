0fdb9a97c6545f7321d08d60ada4c3a22f0b339c
violet-wdream <142767104+violet-wdream@users.noreply.github.com>
2023-09-04 10:59:02 +0800

    Add files via upload
    cpp

---
+++ Main.cpp
@@ -0,0 +1,363 @@
+﻿#include <iostream>
+#include<fstream>
+#include <Windows.h>
+#include <mmsystem.h>
+#include "Bag.h"
+#include "Enemy.h"
+#include "Fight.h"
+#include "Goods.h"
+#include "Map.h"
+#include "Npc.h"
+#include "Role.h"
+#include "Skill.h"
+#include "Store.h"
+#include"Save.h"
+#include"Task.h"
+#include"background.h"
+#include"examinateInput.h"
+#pragma comment (lib,"winmm.lib")
+
+
+using namespace std;
+
+
+
+
+void newGame(Role player, Map map) {
+
+	Goods goods[24] = { 0, 1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 };
+	Store store;
+	int chatNum = 0;// ?
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	background(1);
+
+
+
+menu:
+	Sleep(1);
+	system("cls");
+	player.showRole();
+	cout << endl << "你的位置 :" << map.getName() << endl;
+	map.initNpc(map.getPosition(), map.n);
+	map.showNpc(map.n);                   //show the npc
+	cout << endl;
+
+
+
+
+	cout << "Enemy:" << endl;            //show the enemy
+	if (map.isThereFight()) 
+	{
+		Enemy_Boss  boss(map.getPosition());
+		cout << boss.getName();
+	}
+
+
+
+
+												//show the info of you
+	cout << "\n\n1.交谈 2.战斗 3.地图 4.状态 5.商店  6.保存并退出" << endl;
+	int choice = 1;
+	cin >> choice;
+	choice = examinateInput(1, 6, choice);
+
+	switch (choice)
+	{
+	case 1:
+	{
+		cout << endl << "可对话的人物:" << endl;
+		map.showNpc(map.n);
+		cout << "\n请输入要对话的人物(序号):  "<<endl;
+		int npcNum = 0;
+		cin >> npcNum;
+		npcNum = examinateInput(0, 2, npcNum);
+		map.n[map.getPosition()].chat(map.getPosition(), npcNum);
+		Sleep(1000);
+		break;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	case 2:
+	{
+		if (map.isThereFight())
+		{
+			Enemy_Boss  boss(map.getPosition());
+			Enemy_Small smallEnemy;
+			boss.showEnemy();
+			Fight fight(player, boss);
+			system("cls");
+			cout << "正在进入战斗......." << endl;
+			while (!fight.isFightEnd())
+			{
+				Sleep(3000);
+				system("cls");
+				if (fight.fightRound() == true)
+				{
+					player = fight.runEndFight();
+					goto menu;
+				}
+			}
+			player = fight.endFight();		//战斗正常结束，获得增益
+			smallEnemy.~Enemy_Small();
+		}
+		break;
+	}
+	//----------------------------------------------------------------------------------------------------------------------------------
+	case 3:
+	{
+		while (true)
+		{
+			map.ShowMap();
+			cout << "使用w a s d来移动 按1进入地图" << endl;
+			char order;
+			cin >> order;
+			if (order == '1') {
+				system("cls");
+				background(map.getPosition());
+				break;
+			}
+
+			else {
+				map.Move(order);
+				chatNum = 0;
+				player.setMapId(map.getPosition());
+			}
+
+		}
+		break;
+
+
+	}
+	//----------------------------------------------------------------------------------------------------------------------------------
+	case 4:
+	{
+		cout << "1.背包 2.技能 3.装备 4.任务 5.返回" << endl;
+		int ch1=1;
+		ch1 = examinateInput(1, 5, ch1);
+		switch (ch1)                                                                //ch1
+		{
+			//-------------------------------------------------------------------------------
+			case 1:
+			{
+				player.showBag();		//显示背包
+				player.useDrug();		//是否使用药品
+				break;
+			}
+			//-------------------------------------------------------------------------------
+			case 2:
+			{
+				player.showSkill();
+				break;
+			}
+			//-------------------------------------------------------------------------------
+			case 3:
+			{
+
+				player.showEquip();
+				cout << "1.更换装备		2.取下装备		3.退出" << endl;
+				int ch2 = 1;
+				cin >> ch2;
+				ch2 = examinateInput(1, 3, ch2);               //ch2
+				//--------------------------------
+				if (ch2 == 1)
+				{
+					player.showBag();
+
+					cout << "请选择要换上的装备" << endl;
+					int equipId = 1;
+					cin >> equipId;
+					equipId = examinateInput(1, 24, equipId);
+					player.wearEquip(equipId);
+					player.getBag().reduceGoods(equipId, 1);
+				}
+				//-------------------------------
+				else if (ch2 == 2)
+				{
+					cout << "请输入要换下的装备" << endl;
+					cout << "1." << goods[player.getWeapon()].getName() << endl;
+					cout << "2." << goods[player.getClothes()].getName() << endl;
+					int chEquip;
+					cin >> chEquip;
+					chEquip = examinateInput(1, 2, chEquip);
+					if (chEquip == 1)
+					{
+						player.removeEquip(player.getWeapon());
+						player.getBag().addGoods(player.getWeapon(), 1);
+					}
+					else
+					{
+						player.removeEquip(player.getClothes());
+						player.getBag().addGoods(player.getClothes(), 1);
+					}
+				}
+				//------------------------------
+				else if (ch2 == 3)
+				{
+					goto menu;
+				}
+
+
+				break;
+			}
+			//------------------------------------------------------------------------------
+			case 4:
+			{
+				cout << "任务" << player.getTaskId() << '\t' << player.getTaskName() << '\t' << player.getTaskDesc() << endl << endl;
+				break;
+			}
+			//------------------------------------------------------------------------------
+			case 5:
+			{
+				goto menu;
+				break;
+			}
+			//-----------------------------------------------------------------------------
+			}
+
+		break;
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------------------
+	case 5:
+	{
+		cout << "1.购买物品		2.售出物品		3.退出" << endl;
+		int choiceStore;
+		cin >> choiceStore;
+		choiceStore = examinateInput(1, 3, choiceStore);
+		if (choiceStore == 1)
+		{
+			store.showStores();
+			player = store.storeToPlayer(player);
+		}
+		if (choiceStore == 2)
+		{
+			player.showBag();
+			player = store.playerToStore(player);
+		}
+		if (choiceStore == 3)
+		{
+			goto menu;
+		}
+
+		break;
+
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------
+	
+	case 6:
+	{
+		Save::setToFile(player);
+		exit(0);
+		break;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+
+
+	}
+	goto menu;
+}
+
+
+
+
+
+//读取游戏
+void readFile() {
+	ifstream fileRole("SaveRole.dat", ios_base::in | ios_base::binary);
+	ifstream fileBag("SaveBag.dat", ios_base::in | ios_base::binary);
+	ifstream fileSkill("SaveSkill.dat");
+	ifstream fileTask("SaveTask.dat");
+	if (!fileRole) {
+		cout << "没有保存的游戏！" << endl;
+		cout << "请重新选择:" << endl;
+	}
+	else {
+		string name;
+		int type, health_max, health, magic_max, magic, attack, exp, level, defend, money, mapId, story, weapon, clothes;
+		fileRole >> name >> type >> health_max >> health >> magic >> magic_max >> attack >> exp >> level >> defend >> money >> mapId >> story >> weapon >> clothes;
+
+		Role player(type);
+		player.setHealth_max(health_max);
+		player.setHealth(health);
+		player.setMagic_max(magic_max);
+		player.setMagic(magic);
+		player.setAttack(attack);
+		player.setExpSave(exp);
+		player.setLevelSave(level);
+		player.setDefend(defend);
+		player.setMoney(money);
+		player.setMapId(mapId);
+		player.setStory(story);
+		player.setWeapon(weapon);
+		player.setClothes(clothes);
+
+		Skill skill;
+		fileSkill.read(reinterpret_cast<char *>(&skill), sizeof(Skill));
+		Skill &newSkill = skill;
+		player.setSkill(newSkill);
+
+		Task task(0);
+		fileTask.read(reinterpret_cast<char *>(&task), sizeof(Task));
+		player.setTask(task);
+
+
+		for (int key2 = 24;!fileBag.eof();) {
+			
+			int key;
+			int value;
+			fileBag >> key >> value;
+			if (key2 == key)
+				break;
+			player.addSaveGoodsToBag(key, value);
+			key2 = key;
+
+		}
+		fileRole.close();
+		fileBag.close();
+		fileSkill.close();
+		Map newMap(player.getMapId());
+
+		cout << "读入成功！" << endl;
+		newGame(player,newMap);
+	}
+}
+
+//程序入口
+int main(){
+	welcomePage();
+
+	
+
+
+	int choice;
+	while (true) 
+	{
+		cin >> choice;
+		if (choice != 1 && choice != 2 && choice != 3)
+				cout << "选择错误，请重新选择。" << endl;
+			else break;
+	}
+
+		if (choice == 3)
+			exit(0);
+		//游戏创建
+
+
+		if (choice == 1) 
+		{
+			Role player(1);
+			system("cls");
+			Map map;
+			newGame(player,map);
+		}
+
+
+		if (choice == 2) 
+		{
+			readFile();
+		}
+	return 0;
+}
+
+
+
---
+++ Map.cpp
@@ -0,0 +1,301 @@
+﻿#include <iostream>
+#include "Map.h"
+
+using namespace std;
+
+Map::Map()  
+{
+	position = 1;
+	dx = 1;
+	dy = 0;
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			pos[i][j] = ' ';
+		}
+	}
+	pos[dx][dy] = 'Y';
+}
+
+Map::Map(int p) 
+{
+	position = p;
+	if (p == 0) 
+	{
+		dx = 0;
+		dy = 0;
+	}
+	if (p == 1) 
+	{
+		dx = 1;
+		dy = 0;
+	}
+	if (p == 2) 
+	{
+		dx = 1;
+		dy = 1;
+	}
+	if (p == 3) 
+	{
+		dx = 0;
+		dy = 1;
+	}
+	if (p == 4) 
+	{
+		dx = 0;
+		dy = 2;
+	}
+	if (p == 5) 
+	{
+		dx = 2;
+		dy = 1;
+	}
+	if (p == 6) 
+	{
+		dx = 2;
+		dy = 2;
+	}
+	if (p == 7) 
+	{
+		dx = 1;
+		dy = 2;
+	}
+	
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			pos[i][j] = ' ';
+		}
+	}
+	pos[dx][dy] = 'Y';
+}
+
+
+void Map::Move(char order)
+{
+	pos[dx][dy] = ' ';
+	switch (order)
+	{
+	case 'w': {
+		if (position == 1 || position == 3 || position == 5) {
+			dy++;
+			position++;
+		}
+		else
+		{
+			cout << "移动失败" << endl;
+		}
+		break;
+	}
+	case 'a': {
+		if (position == 3 || position == 7) {
+			dx++;
+			position--;
+		}
+		else if (position == 0)
+		{
+			dx++;
+			position = 1;
+		}
+		else if (position == 2)
+		{
+			dx++;
+			position = 5;
+		}
+		else if (position == 4)
+		{
+			dx++;
+			position = 7;
+		}
+		else
+		{
+			cout << "移动失败" << endl;
+		}
+		break;
+	}
+	case 's': {
+		if (position == 2 || position == 4 || position == 6) {
+			dy--;
+			position--;
+		}
+		else
+		{
+			cout << "移动失败" << endl;
+		}
+		break;
+	}
+	case 'd': {
+		if (position == 2 || position == 6) {
+			dx--;
+			position++;
+		}
+		else if (position == 1)
+		{
+			dx--;
+			position = 0;
+		}
+		else if (position == 5)
+		{
+			dx--;
+			position = 2;
+		}
+		else if (position == 7)
+		{
+			dx--;
+			position = 4;
+		}
+		else
+		{
+			cout << "移动失败" << endl;
+		}
+		break;
+	}
+	default:
+		break;
+	}
+	pos[dx][dy] = 'Y';
+}
+
+//-----------------------------------------------------------------------------------------------------------
+void Map::ShowMap()
+{
+	system("cls");
+	cout << "地图:" << endl;
+	
+	cout << " __________	   __________	     __________ " << endl;
+	cout << "|          |	  |          |	    |          |" << endl;
+	//cout<<"|          |          |          |          |"<<endl;
+	cout << "| 苍白宫廷 |———| 黑卵圣殿 |———| 王后花园 |" << endl;
+	cout << "|     " << pos[2][2] << "    |	  |     " << pos[1][2] << "    |	    |    " << pos[0][2] << "     |" << endl;
+	//cout<<"|          |       |   |          |          |"<<endl;
+	cout << "|__________|	  |__________|	    |__________|" << endl;
+	cout << "      |                                   |            " << endl;
+	cout << "      |                                   |            " << endl;
+	cout << "      |                                   |            " << endl;
+	cout << " _____|____	   __________	     _____|____ " << endl;
+	cout << "|          |	  |          |	    |          |" << endl;
+	//cout<<"|          |          |          |          |"<<endl;
+	cout << "| 水晶山峰 |———|遗忘十字路|———|  螳螂村  |" << endl;
+	cout << "|     " << pos[2][1] << "    |	  |     " << pos[1][1] << "    |	    |    " << pos[0][1] << "     |" << endl;
+	//cout<<"|          |       |   |          |          |"<<endl;
+	cout << "|__________|	  |__________|	    |__________|" << endl;
+	cout << "                        |                             " << endl;
+	cout << "                        |                             " << endl;
+	cout << "                        |                             " << endl;
+	cout << "           	   _____|____	     __________ " << endl;
+	cout << "            	  |          |	    |          |" << endl;
+	//cout<<"|          |          |          |          |"<<endl;
+	cout << "                  | 德特茅斯 |———|   温泉   |" << endl;
+	cout << "             	  |     " << pos[1][0] << "    |	    |    " << pos[0][0] << "     |" << endl;
+	//cout<<"|          |       |   |          |          |"<<endl;
+	cout << "            	  |__________|	    |__________|" << endl;
+
+	
+	cout << "您的位置: " << mapName[position] << endl;
+}
+//----------------------------------------------------------------------------------------------------
+
+
+
+
+void Map::initNpc(int position, Npc npc[3])      //initiate the npc
+{
+	switch (position)
+	{
+		case 0:npc[0].name = "0.奎若"; npc[1].name = "1.泡温泉的小虫子"; npc[2].name = " ";//spring
+			break;
+		case 1:npc[0].name = "0.虫长者"; npc[1].name = "1.伟大的佐特"; npc[2].name = " "; //dirtmouth
+			break;
+		case 2:npc[0].name = "0.米拉"; npc[1].name = " "; npc[2].name = " ";      //crossroads
+			break;
+		case 3:npc[0].name = "0.奎若"; npc[1].name = " ";  npc[2].name = " ";     //mantistown
+			break;
+		case 4:npc[0].name = "0.白色夫人";  npc[1].name = " "; npc[2].name = " ";//queengarden
+			break;
+		case 5:npc[0].name = "0.奎若"; npc[1].name = "1.猎人";   npc[2].name = " ";//crystalpeak
+			break;
+		case 6:npc[0].name = "0.苍白之王沃姆的尸体";  npc[1].name = " "; npc[2].name = " ";//palepalace
+			break;
+		case 7:npc[0].name = "0.黄蜂女";  npc[1].name = " "; npc[2].name = " "; //blackeggpalace 
+			break;
+		default:
+			break;
+	}
+}
+void Map::showNpc(Npc npc[3])
+{
+	cout << "Npc:" << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << npc[i].name << "  ";
+	}
+}
+//-------------------------------------------------------------------------------------------------------
+/*void Map::initEnemy(int position, Enemy enemy[3])
+{
+	switch (position)
+	{
+	case 0:npc[0].name = "0.奎若"; npc[1].name = "1.泡温泉的小虫子"; npc[2].name = " ";//spring
+		break;
+	case 1:npc[0].name = "0.虫长者"; npc[1].name = "1.伟大的佐特"; npc[2].name = " "; //dirtmouth
+		break;
+	case 2:npc[0].name = "0.米拉"; npc[1].name = " "; npc[2].name = " ";      //crossroads
+		break;
+	case 3:npc[0].name = "0.奎若"; npc[1].name = " ";  npc[2].name = " ";     //mantistown
+		break;
+	case 4:npc[0].name = "0.白色夫人";  npc[1].name = " "; npc[2].name = " ";//queengarden
+		break;
+	case 5:npc[0].name = "0.奎若"; npc[1].name = "1.柯尼法";   npc[2].name = " ";//crystalpeak
+		break;
+	case 6:npc[0].name = "0.苍白之王沃姆的尸体";  npc[1].name = " "; npc[2].name = " ";//palepalace
+		break;
+	case 7:npc[0].name = "0.黄蜂女";  npc[1].name = " "; npc[2].name = " "; //blackeggpalace 
+		break;
+	default:
+		break;
+	}
+}
+*/
+
+
+
+
+bool Map::isThereFight()
+{
+	if (position == 1 || position == 3 || position == 4 || position == 5 || position == 6 || position == 7 || position == 8 || position == 9)
+		return true;
+	else return false;
+}
+
+//  ==
+
+
+
+int Map::getPosition()
+{
+	return position;
+}
+
+
+int Map::getNpcGoodsId() {
+	return npc.getGoodsId();
+}
+
+
+
+int Map::getNpcGoodsNum() {
+	return npc.getGoodsNum();
+}
+
+
+
+string Map::getName(){
+	return mapName[position];
+}
+
+
+
+
