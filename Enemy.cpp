51add164adae15b1a3605d5d039c8edafaa27c20
zyl021ouc <143980837+zyl021ouc@users.noreply.github.com>
2023-09-05 14:29:30 +0800

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include "Enemy.h"
 
 using namespace std;
@@ -107,14 +107,48 @@ Enemy_Small::Enemy_Small() {  //小怪统一血量和攻击？
 }
 Enemy_Small::~Enemy_Small(){}
 
+void Enemy_Boss::showinfor(int whatType) {
+	setType(whatType);
+	switch (whatType) {
+	case 2:
+		cout << "\n大型圣巢十字守卫的遗骸，被奇怪的力量复活。仍在本能的驱使下坚守岗位，对抗入侵者。" << endl;
+		cout << "比它们的兄弟更大更强，这些虫子仍然被迫从其他生物的遗骸窃取武器。" << endl;
+		cout << "它们不漫游，也不狩猎，只是在原地守候，有虫子经过的时候就冲出去攻击。" << endl;
+		cout << "							——龙牙哥" << endl;
+		break;
+	case 3:
+		cout << "\n被废黜的螳螂部落领主。背叛了他的姐妹，投入了感染的怀抱。" << endl;
+		cout << "							——螳螂领主" << endl;
+		break;
+	case 4:
+		cout << "\n一位阵亡战士的残梦。王后花园的守护者。" << endl;
+		cout << "							——马尔穆" << endl;
+		break;
+	case 5:
+		cout << "\n水晶山峰的魁梧矿工，被生长的晶体所吞噬。可以从结晶的肢体射出灼热的光束。" << endl;
+		cout << "它从水晶牢狱中向外看时，这个世界对它而言是什么模样？它只能看到光吗？这是它发疯的原因吗？" << endl;
+		cout << "							——水晶守卫" << endl;
+		break;
+	case 6:
+		cout << "\n圣巢王宫的卫兵，可以熟练地挥舞弯爪刃。\n它们用沉重的白色盔甲保护脆弱的身体。" << endl;
+		cout << "							——国王傀儡" << endl;
+		break;
+	case 7:
+		cout << "\n完全成熟的容器，身体内携带着瘟疫之心。" << endl;
+		cout << "							——空洞骑士" << endl;
+		break;
+	default:break;
+	};
+}
 
 
-Enemy_Boss::Enemy_Boss() {}
+Enemy_Boss::Enemy_Boss() {};
+
 Enemy_Boss::Enemy_Boss(int whatType) {
 	setType(whatType);
 	switch (whatType) {
-	case 1: {
-		name = "暗影黑狼";
+	case 2: {
+		name = "龙牙哥";
 		type = 0;
 		health = 150;
 		health_max = 150;
@@ -137,7 +171,7 @@ Enemy_Boss::Enemy_Boss(int whatType) {
 		break;
 	}
 	case 3: {
-		name = "史诗绯红印记树怪";
+		name = "螳螂领主";
 		type = 2;
 		health = 350;
 		health_max = 350;
@@ -160,7 +194,7 @@ Enemy_Boss::Enemy_Boss(int whatType) {
 		break;
 	}
 	case 4: {
-		name = "史诗苍老雕文魔像";
+		name = "马尔穆";
 		type = 3;
 		health = 700;
 		health_max = 700;
@@ -184,7 +218,7 @@ Enemy_Boss::Enemy_Boss(int whatType) {
 		break;
 	}
 	case 5: {
-		name = "传说级远古石甲虫";
+		name = "水晶守卫";
 		type = 4;
 		health = 1000;
 		health_max =1000;
@@ -208,7 +242,7 @@ Enemy_Boss::Enemy_Boss(int whatType) {
 	}
 
 	case 6: {
-		name = "传说级深红锋啄鸟";
+		name = "国王傀儡";
 		type = 5;
 		health = 4000;
 		health_max = 4000;
@@ -231,7 +265,7 @@ Enemy_Boss::Enemy_Boss(int whatType) {
 		break;
 	}
 	case 7: {
-		name = "传说级魔沼蛙";
+		name = "空洞骑士";
 		type = 6;
 		health = 4000;
 		health_max = 4000;
@@ -251,53 +285,6 @@ Enemy_Boss::Enemy_Boss(int whatType) {
 		
 		exp = 15;		//打死该怪物得到的经验
 		money = 40;		//打死该怪物得到的金钱
-		break;
-	}
-	case 8: {
-		name = "炼狱巨龙";
-		type = 7;
-		health = 8000;
-		health_max = 8000;
-		magic = 310;
-		magic_max = 310;
-		attack = 600;
-		defend = 500;
-
-		goodsNum[0] = 2;			//对应每件物品的数量
-		goodsNum[1] = 3;
-		goodsNum[2] = 2;
-		goodsNum[3] = 1;
-		goodsId[0] = 18;	//打死该怪物会掉落的物品
-		goodsId[1] = 19;
-		goodsId[2] = 20;
-		goodsId[3] = 21;
-
-		exp = 30;		//打死该怪物得到的经验
-		money = 110;		//打死该怪物得到的金钱
-		break;
-	}
-	case 9: {
-		name = "最强霸龙纳什男爵";
-		type = 8;
-		health = 12000;
-		health_max = 12000;
-		magic = 510;
-		magic_max = 510;
-		attack = 800;
-		defend = 700;
-
-		goodsNum[0] = 2;			//对应每件物品的数量
-		goodsNum[1] = 3;
-		goodsNum[2] = 2;
-		goodsNum[3] = 1;
-		goodsId[0] = 20;	//打死该怪物会掉落的物品
-		goodsId[1] = 21;
-		goodsId[2] = 22;
-		goodsId[3] = 23;
-
-		
-		exp = 50;		//打死该怪物得到的经验
-		money = 500;		//打死该怪物得到的金钱
 		break;
 	}
 	default:break;
